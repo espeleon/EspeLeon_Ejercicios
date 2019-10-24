ddd30d73aa07c440ba92e120ad959c4085223204
espeleon <espe_coo@hotmail.com>
2019-10-24 10:13:50 +0200

    Ejercicios Variables, Constantes y Bucle For

---
+++ Variables_Constantes_Bucle/C#/Fase1/Fase1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             • Crea tres variables string e inicialitzales amb les dades pertinents (nom, cognom1, cognom2).
+             • Crea tres variables int e inicialitzales amb les dades pertinents (dia, mes, any).
+             • Mostra per pantalla les variables string concatenant-les en aquest ordre (cognom1 + cognom2, + nom).
+             • Mostra per pantalla les variables int concatenant-les amb “/” entre cada una d’elles.
+             */
+
+            string sNom = "Espe";
+            string sCognom1 = "León";
+            string sCognom2 = "Hidalgo";
+            int iDia = 22;
+            int iMes = 10;
+            int iAny = 2019;
+
+            // string: another way of do it 
+
+            var sb = new StringBuilder();
+            sb.Append ( "Espe ");
+            sb.Append ( "León ") ;
+            sb.Append("Hidalgo");
+
+
+            Console.WriteLine("Mi nombre es: "+sNom + " " + sCognom1+ " " + sCognom2);
+            Console.WriteLine(@"Y hoy es día: " + iDia + "/" + iMes + "/" + iAny);
+
+            // result with StringBuilder
+            Console.WriteLine("Este es con el StringBuilder: " +sb.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Variables_Constantes_Bucle/C#/Fase4/Fase4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace Fase4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            • Creeu una variable on juntareu el nom i els cognoms(tot en una variable) i un altre on juntareu la data de naixement
+            separada per “/” (tot en una variable).
+            Mostreu per consola les variables del nom complet, la data de naixement i si l’any de naixement es de traspàs o no.
+            */
+
+            var sb = new StringBuilder();
+            sb.AppendLine("El meu nom es Espe León Hidalgo.");
+            sb.AppendLine(@"Vaig nèixer el 14/11/1974.");
+            sb.AppendLine("El meu any de naixament NO es de trespàs.");
+
+
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
